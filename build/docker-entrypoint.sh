1dfb2735b1c62f986171a8c82ad7269baa399929
azzurri1934 <azzurri1934@gmail.com>
2019-05-26 01:06:55 +0900

    refactoring

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -5,52 +5,66 @@ DOKUWIKI_ROOT=$DOCUMENT_ROOT/dokuwiki
 FARM_ROOT=$DOCUMENT_ROOT/farm
 APACHE_ROOT=/etc/apache2
 
-DOKUWIKI_TAR_FILE=release_stable_2018-04-22b.tar.gz
-DOKUWIKI_FARM_ANIMAL_ZIP_FILE=dokuwiki_farm_animal.zip
-
+FARM1=azzu
+FARM2=majo
 SUB_DOMAIN=wiki.azzu.mydns.jp
+CONFIGURED_FLAG=configured
 
-if [ -z "$(ls $DOCUMENT_ROOT)" ]; then
-  apk update
-  apk add --no-cache apache2 php7-apache2 php7-mbstring php7-session php7-json php7-xml php7-openssl openrc
-  
-  mkdir $DOKUWIKI_ROOT
-  wget https://github.com/splitbrain/dokuwiki/archive/$DOKUWIKI_TAR_FILE
-  tar -zxvf $DOKUWIKI_TAR_FILE -C $DOKUWIKI_ROOT --strip-components 1
-  rm $DOKUWIKI_TAR_FILE
-  
-  mkdir $FARM_ROOT
-  wget https://www.dokuwiki.org/_media/$DOKUWIKI_FARM_ANIMAL_ZIP_FILE
-  unzip $DOKUWIKI_FARM_ANIMAL_ZIP_FILE -d $FARM_ROOT
-  rm $DOKUWIKI_FARM_ANIMAL_ZIP_FILE
+# Configure Dokuwiki, Dokuwiki Farms and Apache.
+if [ ! -f $CONFIGURED_FLAG ]; then
+ 
+    # Configure the DokuWiki timezone, e.g. Asia/Tokyo.
+    sed -i "s/date_default_timezone_set(@date_default_timezone_get());/date_default_timezone_set('Asia\/Tokyo');/g" $DOKUWIKI_ROOT/inc/init.php
 
-  cp $DOKUWIKI_ROOT/inc/preload.php.dist $DOKUWIKI_ROOT/inc/preload.php
-  sed -i "s/\/\/if(!defined('DOKU_FARMDIR'))/if(!defined('DOKU_FARMDIR'))/g" $DOKUWIKI_ROOT/inc/preload.php
-  sed -i "s/\/var\/www\/farm/\/var\/www\/localhost\/htdocs\/farm/g" $DOKUWIKI_ROOT/inc/preload.php
-  sed -i "s/\/\/include(fullpath(dirname(__FILE__))/include(fullpath(dirname(__FILE__))/g" $DOKUWIKI_ROOT/inc/preload.php
+    # Activate DokuWiki Farms.
+    cp $DOKUWIKI_ROOT/inc/preload.php.dist $DOKUWIKI_ROOT/inc/preload.php
+    sed -i "s/\/\/if(!defined('DOKU_FARMDIR'))/if(!defined('DOKU_FARMDIR'))/g" $DOKUWIKI_ROOT/inc/preload.php
+    sed -i "s/\/var\/www\/farm/\/var\/www\/localhost\/htdocs\/farm/g" $DOKUWIKI_ROOT/inc/preload.php
+    sed -i "s/\/\/include(fullpath(dirname(__FILE__))/include(fullpath(dirname(__FILE__))/g" $DOKUWIKI_ROOT/inc/preload.php
 
-  sed -i "s/date_default_timezone_set(@date_default_timezone_get());/date_default_timezone_set('Asia\/Tokyo');/g" $DOKUWIKI_ROOT/inc/init.php
+    # Configure Dokuwiki Farms, e.g. $FARM1(azzu) and $FARM2(majo).
+    # Configure the $FARM1(azzu) farm.
+    cp -pr $FARM_ROOT/_animal/ $FARM_ROOT/$FARM1/
+    sed -i 's/\/\/$conf/$conf/g' $FARM_ROOT/$FARM1/conf/local.protected.php
+    sed -i "s/\/farm\/animal/\/$FARM1/g" $FARM_ROOT/$FARM1/conf/local.protected.php
 
-  sed -i "s/#LoadModule rewrite_module modules/LoadModule rewrite_module modules/g" $APACHE_ROOT/httpd.conf
-  sed -i "s/Options Indexes FollowSymLinks/Options FollowSymLinks/g" $APACHE_ROOT/httpd.conf
+    # Configure the $FARM2(majo) farm.
+    cp -pr $FARM_ROOT/_animal/ $FARM_ROOT/$FARM2/
+    sed -i 's/\/\/$conf/$conf/g' $FARM_ROOT/$FARM2/conf/local.protected.php
+    sed -i "s/\/farm\/animal/\/$FARM2/g" $FARM_ROOT/$FARM2/conf/local.protected.php
 
-  cat <<EOL >> $APACHE_ROOT/httpd.conf
+    # Configure the $FARM1(azzu) farm alias and the $FARM2(majo) farm alias.
+    cat <<EOL >> $APACHE_ROOT/httpd.conf
 
-<Directory $FARM_ROOT>
-      AllowOverride All
-      Options +FollowSymLinks
-
-      RewriteEngine On
+<IfModule alias_module>
+    Alias /$FARM1 $FARM_ROOT/$FARM1
+    Alias /$FARM2 $FARM_ROOT/$FARM2
+</IfModule>
 EOL
 
-  cat <<"EOL" >> $APACHE_ROOT/httpd.conf
+    # Configure Apache for DokuWiki Farms.
+    sed -i "s/#LoadModule rewrite_module modules/LoadModule rewrite_module modules/g" $APACHE_ROOT/httpd.conf
 
-      RewriteRule ^/?([^/]+)/(.*)  /dokuwiki/$2?animal=$1 [QSA]
-      RewriteRule ^/?([^/]+)$      /dokuwiki/?animal=$1 [QSA]
+    cat <<EOL >> $APACHE_ROOT/httpd.conf
+
+<Directory $FARM_ROOT>
+    AllowOverride All
+    Options +FollowSymLinks
+
+    RewriteEngine On
+EOL
+
+    cat <<"EOL" >> $APACHE_ROOT/httpd.conf
+
+    RewriteRule ^/?([^/]+)/(.*)  /dokuwiki/$2?animal=$1 [QSA]
+    RewriteRule ^/?([^/]+)$      /dokuwiki/?animal=$1 [QSA]
 </Directory>
 EOL
 
-  cat <<EOL >> $APACHE_ROOT/httpd.conf
+    # Configure other Apache settings.
+    sed -i "s/Options Indexes FollowSymLinks/Options FollowSymLinks/g" $APACHE_ROOT/httpd.conf
+
+    cat <<EOL >> $APACHE_ROOT/httpd.conf
 
 <VirtualHost *:80>
     ServerName any
@@ -65,7 +79,9 @@ EOL
 </VirtualHost>
 EOL
 
-  chown -R apache:apache $DOCUMENT_ROOT
+    # Turn on the configured flag.
+    touch $CONFIGURED_FLAG
 fi
 
+# Start Apache.
 httpd -D FOREGROUND
